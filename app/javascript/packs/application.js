fe3a12e1e654d87b308ff41bbf91711479074cb4
bulchack <jesuseovalles@gmail.com>
2022-01-03 10:37:20 -0300

    add deatils to the app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,21 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+window.scroll_bottom = function() {
+    if( $('#messages').length > 0 ) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    };
+};
+
+const submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+        if( e.keyCode == 13 ) {
+            $('button').click();
+            e.target.value = "";
+        };
+    });
+};
+
 $(document).on("turbolinks:load", () => {
     $(".ui.dropdown").dropdown();
     $('.message .close').on('click', function() {
@@ -21,6 +36,7 @@ $(document).on("turbolinks:load", () => {
         .closest('.message')
         .transition('fade')
         ;
-    })
-;
+    });
+    submit_message();
+    scroll_bottom();
 });
